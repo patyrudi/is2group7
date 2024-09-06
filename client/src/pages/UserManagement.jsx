87e017a810d9a0ff6336ebf4fc6085171e6aae7f
patyrudi <patyrudica@gmail.com>
2024-09-06 10:49:00 -0400

    pequeños ajustes

--- client/src/pages/UserManagement.jsx
+++ client/src/pages/UserManagement.jsx
@@ -1,6 +1,6 @@
 import { NavBar } from "../components/NavBar";
 import {useEffect, useState} from "react";
-import {getAll} from "../api/usuarios.api";
+import {getAllUsers} from "../api/usuarios.api";
 
 export function UserManagement() {
   return (
